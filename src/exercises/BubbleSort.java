3d7c87e7eabe2191c92781e7c53d006ba6c0dc09
iuri <iuri@10.0.1.109>
2017-09-11 13:22:20 -0300

    ++

--- src/exercises/BubbleSort.java
+++ src/exercises/BubbleSort.java
@@ -1,36 +1,66 @@
 package exercises;
 
-public class BubbleSort {
+import java.util.Arrays;
+import java.util.List;
 
-	public static void main (String args[]) {
+public class BubbleSort {
+	
+	
+	public static void main(String args[]) {
+		BubbleSort bu = new BubbleSort();
 		
-		int array[] = new int[] {1,6,3,8,45,6,3,2};
-		int temp;		
-		int iterations = 0;		
-		boolean hasChange = false;
+		// List
+		//List<Integer> list = Arrays.asList(2,67,3,4,9,2,3,1);
+		//System.out.println(bu.bubbleList(list));
 		
-		for(int i=0;i<array.length;i++) {			
+		// Array
+		int array[] = {4,2,6,3,8,7,9};
+		int result[] = bu.bubbleArray(array); 
+		System.out.print(Arrays.toString(result));
+		
+	}
+	
+	public List<Integer> bubbleList(List<Integer> list) {
+		
+		int it = 0;
+		for(int i=0;i<list.size();i++) {
 			
-			iterations++;
-			for(int j=0;j<array.length-1; j++) {
-				if(array[j]>array[j+1]) {
+			boolean hasChange = false;
+			for(int j=0;j<(list.size()-1);j++) {
+				it++;
+				if(list.get(j)>list.get(j+1)) {
 					hasChange = true;
-					temp = array[j+1];
-					array[j+1] = array[j];
-					array[j] = temp;
+					int temp = list.get(j+1);
+					list.set(j+1, list.get(j));
+					list.set(j, temp);
 				}
 			}
-			
 			if(!hasChange) {
 				break;
 			}
 		}
-		
-		System.out.println("Iterations: "+iterations);
-		for(int k=0; k<array.length; k++) {
-			System.out.print(array[k]+" ");
-		}
-		
+		System.out.println("It: "+ it);
+		return list;
 	}
 	
+	public int[] bubbleArray(int[] array) {
+		int it = 0;
+		for(int i=0; i< array.length; i++) {
+			boolean hasChange = false;
+			for(int j=0;j< (array.length-1);j++ ) {
+				it++;
+				if (array[j]>array[j+1]) {
+					hasChange = true;
+					int temp = array[j+1];
+					array[j+1] = array[j];
+					array[j] = temp;
+				}
+			}
+			if(!hasChange) {
+				break;
+			}
+		}
+		System.out.println("It: "+ it);
+		return array;
+	}
 }
--- src/exercises/Functions.java
+++ src/exercises/Functions.java
@@ -1,9 +1,18 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Functions {
 	public static void main(String args[]) {
 		Functions main = new Functions();
-		int result = main.somarDig(123);
+		
+		// boolean result = main.compare("iuri", "iuri");
+		// List<Integer> result = main.loadListAsc(1, 5);
+		
+		float result = main.listSumContent(Arrays.asList(1,2,3,4));
+		
 		System.out.println(result);
 	}
 	
@@ -32,7 +41,12 @@ public class Functions {
 		
 	}
 	
+	public boolean compare(String a, String b) {
+		return a.equals(b);
+	}
+	
 	public int somarDig(int dig) {
+
 		
 		int soma = 0;
 		while(dig!=0) {
@@ -44,4 +58,37 @@ public class Functions {
 		return soma;
 		
 	}
+	
+	public List<Integer> loadListAsc(int ini, int end){
+		
+		List<Integer> list = new ArrayList<Integer>();
+		while(ini<=end) {
+			list.add(ini);
+			ini++;
+		}
+		return list;
+	}
+	
+	public float listSumContent(List<Integer> list) {
+		float result = 0;
+		
+		if(!list.isEmpty()) {
+			int size = list.size();
+			for(int i=0;i<size;i++) {
+				result += list.get(i);
+			}
+		}	
+		
+		return result;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
