59b25d3bb8ae4467b67a4d356e56a2b97d732de4
iuri <iuri@10.0.1.109>
2018-04-10 13:22:54 -0300

    Test01

---
+++ src/test01/Test01.java
@@ -0,0 +1,54 @@
+package test01;
+
+public class Test01 {
+
+	public static void main(String args[]) {
+		
+		// Google Drive: Estudos Livros > Java > exercises-java - Test01
+		
+		int [] ar = {-1,0,-3};
+		// int [] ar = {1, 5, 2, 3, 2, 3, 3};
+		Test01.test01(ar);
+		
+	}
+	
+	public static int test01(int [] ar) {
+		
+		// order (bubble)
+		int n = ar.length;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n-1;j++){
+                if(i!=j){
+                    if(ar[j]>ar[j+1]){
+                        int temp = ar[j+1];
+                        ar[j+1] = ar[j];
+                        ar[j] = temp;
+                        // Print here to see each step //
+                    }                    
+                }
+            }            
+        }
+        
+//        for(int k=0;k<n;k++){
+//          System.out.print(ar[k]+" ");
+//        }
+        
+        int small = ar[0]+1; // considering N is between 1 and 100k
+        if(small==0) {
+        		small++;
+        }
+        
+		for(int i=1;i<ar.length;i++) {		
+			
+			if(small>=ar[i]) {
+				small = ar[i]+1;
+			}
+			
+		}
+		
+		System.out.println(small);
+		
+		return 0;
+	}
+	
+}
